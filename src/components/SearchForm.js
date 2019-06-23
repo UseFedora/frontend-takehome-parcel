ef05e18202f035a96c4acc15bc00becbb2809cbf
JenLovesCode <jenpaulino3@gmail.com>
2019-06-23 00:56:15 -0400

    installed axios and performed a get request to api that populates searchResults array property with results from entered value

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,9 +1,47 @@
 import React, { Component } from "react";
+import axios from "axios";
 
 class SearchForm extends Component {
-  state = {};
+  state = {
+    value: "",
+    searchResults: []
+  };
+
+  handleChange = event => {
+    this.setState({
+      value: event.target.value
+    });
+  };
+
+  handleSubmit = event => {
+    event.preventDefault();
+    axios
+      .get(`http://localhost:3000/api/v1/search.json?query=${this.state.value}`)
+      .then(response => {
+        this.setState({
+          searchResults: response.data
+        });
+        return response;
+      })
+      .catch(error => {
+        return error;
+      });
+  };
+
   render() {
-    return <p>Search Form</p>;
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Name:
+          <input
+            type="text"
+            value={this.state.value}
+            onChange={this.handleChange}
+          />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
+    );
   }
 }
 

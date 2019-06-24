bc12fd92cbc7b6ee3622fbf913bc864117875430
JenLovesCode <jenpaulino3@gmail.com>
2019-06-24 03:14:24 -0400

    installed node-sass and updated scss rendering

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import Search from "./src/views/Search";
+import Search from "./views/Search";
+import "../src/app.scss";
 
 const element = <Search />;
 ReactDOM.render(element, document.getElementById("app"));
